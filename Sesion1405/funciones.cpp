f69ed399c8e79c22aa5d83164cb3307cc14fbe2d
Oalvarado031 <alvaradoo@uamv.edu,ni>
2024-05-24 10:06:03 -0600

    Ejercicios1405

---
+++ Sesion1405/funciones.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+/*Funciones*/
+using namespace std;
+/*Saludar*/
+void showMessage();
+void sumaDos();
+
+
+void showMessage()
+{
+    printf("Universidad Americana \n");
+    printf("UAM \n");
+}
+
+void sumaDos()
+{
+    int num1 = 5, num2 = 10;
+    int res;
+    res = num1 + num2;
+    cout << res << endl;
+}
---
+++ Sesion1405/principal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "funciones.cpp"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    showMessage();
+    sumaDos();
+    return 0;
+}
